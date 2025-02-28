7bb929e294b2857b8e755b972746063a876a5e14
lui1zz <luiz0921341@gmail.com>
2025-02-28 18:08:09 -0300

    resolvendo desafio listartarefa

--- Collections-java/src/OperacoesBasicas/ListaTarefa.java
+++ Collections-java/src/OperacoesBasicas/ListaTarefa.java
@@ -31,4 +31,27 @@ public class ListaTarefa {
     public void obterDescricoesTarefas(){
         System.out.println(tarefaList);
     }
+
+    public static void main(String[] args){
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas() );
+
+        listaTarefa.adicionarTarefa("tarefa 1");
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas() );
+
+        
+        listaTarefa.adicionarTarefa("tarefa 1");
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas() );
+
+        
+        listaTarefa.adicionarTarefa("tarefa 2");
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas() );
+
+        listaTarefa.removerTarefa("tarefa 1");
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas() );
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+
 }
